350c19182f0bff40ed92581169e233344f6afec8
HichDh <57331622+HichDh@users.noreply.github.com>
2022-04-12 02:17:35 +0200

    Update ros_entrypoint.sh

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8be7240b2d236b8193771cea8852f60ccc260c0c4299b2ed0fb6f0609525a56d
-size 370
+#!/bin/bash
+set -e
+
+echo "ROS_DISTRO $ROS_DISTRO"
+echo "ROS_ROOT   $ROS_ROOT"
+PWD="$(pwd)"
+
+ROS_ENV_SETUP="/opt/ros/$ROS_DISTRO/setup.bash"
+ROS_WORKSPACE_SETUP="$PWD/devel/setup.bash"
+
+echo "Source ROS_ENV_SETUP  $ROS_ENV_SETUP"
+source "$ROS_ENV_SETUP"
+
+echo "Source ROS_WORKSPACE_SETUP  $ROS_WORKSPACE_SETUP"
+source "$ROS_WORKSPACE_SETUP"
+
+# to start terminal
+exec "$@"
